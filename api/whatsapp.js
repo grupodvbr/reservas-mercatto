6324120465df52827577022cc26c7bd5bd0665ee
grupodvbr <grupodvbr@gmail.com>
2026-03-12 17:24:28 -0300

    Update whatsapp.js

--- api/whatsapp.js
+++ api/whatsapp.js
@@ -9,8 +9,8 @@ const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE
 )
-const ADMIN_PHONE = "77998253249"
-module.exports = async function handler(req,res){
+const ADMIN_PHONE = "557798253249"
+  module.exports = async function handler(req,res){
 
 /* ================= WEBHOOK VERIFY ================= */
 
