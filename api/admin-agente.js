355441ce5658603116d0e3dcec8bf1e1c07e2ff8
grupodvbr <grupodvbr@gmail.com>
2026-04-23 04:37:48 -0300

    Update admin-agente.js

--- api/admin-agente.js
+++ api/admin-agente.js
@@ -34,7 +34,6 @@ const ADMIN_TOKEN = process.env.ADMIN_TOKEN
 
 const USUARIOS = {
   "557798253249": { nivel: 0 }, // ADMIN REAL
-  "557799761436": { nivel: 0 }, // ADMIN REAL
   "778888888888": { nivel: 1 },
   "777777777777": { nivel: 2, empresa: "MERCATTO DELÍCIA" },
   "776666666666": { nivel: 3 }
